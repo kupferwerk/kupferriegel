46700be2d9da573a718731303fc47fcca5472d0b
Christopher Schott <chsc@kupferwerk.net>
2015-06-06 14:35:25 +0200

    integrate DeviceController in Detector

--- mobile/src/main/java/com/kupferwerk/kupferriegel/detection/DetectorResult.java
+++ mobile/src/main/java/com/kupferwerk/kupferriegel/detection/DetectorResult.java
@@ -2,4 +2,13 @@ package com.kupferwerk.kupferriegel.detection;
 
 public class DetectorResult {
    private int detectorType;
+
+   public int getDetectorType() {
+      return detectorType;
+   }
+
+   public DetectorResult setDetectorType(int detectorType) {
+      this.detectorType = detectorType;
+      return this;
+   }
 }
