baf6e149863f69c999b735926fec2b22c2722ea4
Florian Fetzer <florian.fetzer@kupferwerk.com>
2015-06-06 11:55:59 +0200

    sending simple data on startup

--- mobile/src/main/java/com/kupferwerk/kupferriegel/MainActivity.java
+++ mobile/src/main/java/com/kupferwerk/kupferriegel/MainActivity.java
@@ -5,12 +5,50 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity
+      implements DataApi.DataListener, GoogleApiClient.ConnectionCallbacks,
+      GoogleApiClient.OnConnectionFailedListener {
+
+   private GoogleApiClient apiClient;
+
+   @Override
+   public void onConnected(Bundle bundle) {
+      PutDataMapRequest mapRequest = PutDataMapRequest.create("/wunderbar");
+      mapRequest.getDataMap().putString("Hello", "Hello from my phone");
+
+      PutDataRequest putDataReq = mapRequest.asPutDataRequest();
+      PendingResult<DataApi.DataItemResult> pendingResult =
+            Wearable.DataApi.putDataItem(apiClient, putDataReq);
+   }
+
+   @Override
+   public void onConnectionFailed(ConnectionResult connectionResult) {
+
+   }
+
+   @Override
+   public void onConnectionSuspended(int i) {
+
+   }
 
    @Override
    public boolean onCreateOptionsMenu(Menu menu) {
       getMenuInflater().inflate(R.menu.menu_main, menu);
       return true;
+   }
+
+   @Override
+   public void onDataChanged(DataEventBuffer dataEventBuffer) {
+
    }
 
    @Override
@@ -26,5 +64,19 @@ public class MainActivity extends Activity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+      this.apiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this).addApi(Wearable.API).build();
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+      apiClient.connect();
+   }
+
+   @Override
+   protected void onPause() {
+      super.onPause();
+      apiClient.disconnect();
    }
 }
