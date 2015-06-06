6c3f31a3d535f44f0b0b2c048fb52b5e991a2e19
Sarah Will <swil@kupferwerk.net>
2015-06-06 11:12:04 +0200

    Added user info handling

--- mobile/src/main/java/com/kupferwerk/kupferriegel/user/UserController.java
+++ mobile/src/main/java/com/kupferwerk/kupferriegel/user/UserController.java
@@ -2,14 +2,20 @@ package com.kupferwerk.kupferriegel.user;
 
 import android.app.Activity;
 
+import com.kupferwerk.kupferriegel.utils.RxUtils;
+
 import io.relayr.RelayrSdk;
 import io.relayr.model.User;
 import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 
 public class UserController {
 
    private final Activity activity;
+   private Subscription userInfoSubscription;
+   private User user;
 
    public UserController(Activity activity) {
       this.activity = activity;
@@ -21,5 +27,49 @@ public class UserController {
 
    public void logOut() {
       RelayrSdk.logOut();
+   }
+
+   private void loadUserInfo() {
+      userInfoSubscription =
+            RelayrSdk.getRelayrApi().getUserInfo().observeOn(AndroidSchedulers.mainThread())
+                  .subscribe(new Subscriber<User>() {
+                     @Override
+                     public void onCompleted() {
+                     }
+
+                     @Override
+                     public void onError(Throwable e) {
+                        e.printStackTrace();
+                     }
+
+                     @Override
+                     public void onNext(final User user) {
+                        UserController.this.user = user;
+                     }
+                  });
+   }
+
+   public User getUser() {
+      return user;
+   }
+
+   public void onResume() {
+      resubsribeUserInfo();
+   }
+
+   private void resubsribeUserInfo() {
+      if (RelayrSdk.isUserLoggedIn()) {
+         loadUserInfo();
+      }
+   }
+
+   public void onPause() {
+      unsubscribeUserInfo();
+   }
+
+   private void unsubscribeUserInfo() {
+      if (RxUtils.isSubscribed(userInfoSubscription)) {
+         userInfoSubscription.unsubscribe();
+      }
    }
 }
