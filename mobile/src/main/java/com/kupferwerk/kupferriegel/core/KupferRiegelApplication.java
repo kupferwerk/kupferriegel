5ebd83fee5bd5426999d19f296c92c19afd833c9
Patrick Dorn <pdorn@kupferwerk.net>
2015-06-06 10:57:47 +0200

    mock flavor

--- mobile/src/main/java/com/kupferwerk/kupferriegel/core/KupferRiegelApplication.java
+++ mobile/src/main/java/com/kupferwerk/kupferriegel/core/KupferRiegelApplication.java
@@ -2,12 +2,15 @@ package com.kupferwerk.kupferriegel.core;
 
 import android.app.Application;
 
+import com.kupferwerk.kupferriegel.R;
+
 import io.relayr.RelayrSdk;
 
 public class KupferRiegelApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        new RelayrSdk.Builder(this).build();
+        boolean isMockMode = getResources().getBoolean(R.bool.isMockMode);
+        new RelayrSdk.Builder(this).inMockMode(isMockMode).build();
     }
 }
